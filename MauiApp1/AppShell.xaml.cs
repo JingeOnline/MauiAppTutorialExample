a7d85d9efd35cfa351039997be0ff61fb81d5abe
Jinge <jinge_windows@hotmail.com>
2022-10-12 17:11:07 +1100

    Add project files.

---
+++ MauiApp1/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        //注册导航路由
+        //第一个参数是要注册的 URI 的字符串名称，在本例中解析的名称为 "NotePage"。
+        //第二个参数是导航到时 "NotePage" 要加载的页面的类型。
+        Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+    }
+}
